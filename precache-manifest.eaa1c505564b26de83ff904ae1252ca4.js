dde4396f4ce304723b774e7e160ad3cd99989f22
Sandesh Kumar K S <sandeshkumar555dreams@gmail.com>
2021-05-13 23:50:32 +0530

    Updates

---
+++ precache-manifest.eaa1c505564b26de83ff904ae1252ca4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102fea9f9d787b39d0f27c3154572063",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "8dd2d1aa30930bcc4fe0",
+    "url": "/covid19/static/css/main.1f3337ba.chunk.css"
+  },
+  {
+    "revision": "905613c6267ef13780db",
+    "url": "/covid19/static/js/2.922caa82.chunk.js"
+  },
+  {
+    "revision": "f5de7118f11c3e4b14b47a321484f58a",
+    "url": "/covid19/static/js/2.922caa82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dd2d1aa30930bcc4fe0",
+    "url": "/covid19/static/js/main.1a7d26fd.chunk.js"
+  },
+  {
+    "revision": "1ed7c1ea37003735b645",
+    "url": "/covid19/static/js/runtime-main.6a3465d5.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19/static/media/image.d7265326.png"
+  }
+]);
